d37b40e16bd2d446cb32b949c23be0c2b8ba3936
Lucas Huynh <lqh6vem@virginia.edu>
2026-04-21 13:11:01 -0400

    Similar analysis as before but with updated broader twitter scrape.

--- 2025-2026 FAA Tweets/json_to_csv.py
+++ 2025-2026 FAA Tweets/json_to_csv.py
@@ -1,8 +1,8 @@
 import json
 import pandas as pd
 
-with open("tweets.json", "r", encoding="utf-8") as f:
+with open("2025-2026 FAA Tweets/tweets_updated.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 df = pd.json_normalize(data)
-df.to_csv("tweets_2025.csv", index=False)
+df.to_csv("2025-2026 FAA Tweets/tweets_updated.csv", index=False)
